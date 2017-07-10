fc1070c6edc2e63c95014e6b083ba55073df1716
Enrico Jorns <ejo@pengutronix.de>
2017-07-10 19:01:52 +0200

    src: support for converting classic bundles to casync bundles
      rauc convert classic-bunlde.raucb casync-bundle.raucb
    
    This will generate a casyn-based bundle with the following format
    
    [identifier] (8 bytes)
    [manifest]   (n bytes)
    [caidx]      ([caidx size] bytes)
    [caidx size] (8 bytes)
    [siganture]  ([sig size] bytes)
    [sig size]   (8 bytes)
    
    Signed-off-by: Enrico Jorns <ejo@pengutronix.de>

--- src/main.c
+++ src/main.c
@@ -311,6 +311,52 @@ out:
 	return TRUE;
 }
 
+static gboolean convert_start(int argc, char **argv)
+{
+	RaucBundle *bundle = NULL;
+	GError *ierror = NULL;
+	g_debug("convert start");
+
+	if (r_context()->certpath == NULL ||
+	    r_context()->keypath == NULL) {
+		g_printerr("Cert and key files must be provided\n");
+		r_exit_status = 1;
+		goto out;
+	}
+
+	if (argc < 3) {
+		g_printerr("An input bundle must be provided\n");
+		r_exit_status = 1;
+		goto out;
+	}
+
+	if (argc != 4) {
+		g_printerr("An output bundle name must be provided\n");
+		r_exit_status = 1;
+		goto out;
+	}
+
+	g_debug("input bundle: %s", argv[2]);
+	g_debug("output bundle: %s", argv[3]);
+
+	if (!check_bundle(argv[2], &bundle, TRUE, &ierror)) {
+		g_printerr("%s\n", ierror->message);
+		g_clear_error(&ierror);
+		r_exit_status = 1;
+		goto out;
+	}
+
+	if (!create_casync_bundle(bundle, argv[3], &ierror)) {
+		g_printerr("Failed to create bundle: %s\n", ierror->message);
+		g_clear_error(&ierror);
+		r_exit_status = 1;
+		goto out;
+	}
+
+out:
+	return TRUE;
+}
+
 static gboolean checksum_start(int argc, char **argv)
 {
 	GError *error = NULL;
@@ -972,6 +1018,7 @@ typedef enum  {
 	INSTALL,
 	BUNDLE,
 	RESIGN,
+	CONVERT,
 	CHECKSUM,
 	STATUS,
 	INFO,
@@ -1037,6 +1084,7 @@ static void cmdline_handler(int argc, char **argv)
 		{INSTALL, "install", "install <BUNDLE>", "Install a bundle", install_start, install_group, FALSE},
 		{BUNDLE, "bundle", "bundle <INPUTDIR> <BUNDLENAME>", "Create a bundle from a content directory", bundle_start, NULL, FALSE},
 		{RESIGN, "resign", "resign <BUNDLENAME>", "Resign an already signed bundle", resign_start, NULL, FALSE},
+		{CONVERT, "convert", "convert <INBUNDLE> <OUTBUNDLE>", "Convert a bundle", convert_start, NULL, FALSE},
 		{CHECKSUM, "checksum", "checksum <DIRECTORY>", "Deprecated", checksum_start, NULL, FALSE},
 		{INFO, "info", "info <FILE>", "Print bundle info", info_start, info_group, FALSE},
 		{STATUS, "status", "status", "Show system status", status_start, status_group, TRUE},
@@ -1060,6 +1108,7 @@ static void cmdline_handler(int argc, char **argv)
 			"List of rauc commands:\n" \
 			"  bundle\tCreate a bundle\n" \
 			"  resign\tResign an already signed bundle\n" \
+			"  convert\tConvert classic to casync bundle\n" \
 			"  checksum\tUpdate a manifest with checksums (and optionally sign it)\n" \
 			"  install\tInstall a bundle\n" \
 			"  info\t\tShow file information\n" \
