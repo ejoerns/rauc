179c82891918077981352d1f3100315fa23e3f36
Enrico Joerns <ejo@pengutronix.de>
2021-10-28 13:45:35 +0200

    test case with tmpdir

--- test/test.sh
+++ test/test.sh
@@ -8,27 +8,32 @@ for run in $(seq 1 $1); do
 
 echo "Starting test run $run..."
 
+TMPDIR=$(mktemp -d)
+
 # create target device
-dd if=/dev/zero of=target-dev bs=1M count=10
-mkfs.ext4 -F -I 256 target-dev > /dev/null
+dd if=/dev/zero of=$TMPDIR/target-dev bs=1M count=10
+mkfs.ext4 -F -I 256 $TMPDIR/target-dev > /dev/null
 # create test image
-dd if=/dev/random of=test-image bs=1M count=20
-mkfs.ext4 -F -I 256 test-image > /dev/null
+dd if=/dev/random of=$TMPDIR/test-image bs=1M count=20
+mkfs.ext4 -F -I 256 $TMPDIR/test-image > /dev/null
 
 # simulate installation
 
 # mount and inspect for status file
-mount -t ext4 -v target-dev /mnt
-stat /mnt/rauc.slots || true
-umount -v target-dev
+mkdir $TMPDIR/mount
+mount -t ext4 -v $TMPDIR/target-dev $TMPDIR/mount
+stat $TMPDIR/mount/rauc.slots || true
+umount -v $TMPDIR/target-dev
 
 # copy content of image
-cat test-image > target-dev
+cat $TMPDIR/test-image > $TMPDIR/target-dev
 
 # remount for writing status file
-mount -t ext4 -v target-dev /mnt
-umount -v target-dev
+mount -t ext4 -v $TMPDIR/target-dev $TMPDIR/mount
+umount -v $TMPDIR/target-dev
 
 echo "Test run $run done..."
+
+rm -rf $TMPDIR
 
 done
