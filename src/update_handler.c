3f457f7fa117d4119a78e4837c900796a368c686
Lars Poeschel <poeschel@lemonage.de>
2021-11-11 12:16:23 +0100

    src/update_handler.c: Add fine grained progress for un-tar-ing images
    This adds a more fine grained progress when copying tar images to update
    slots.
    It changes a few things in untar_image function.
    1. The tar subprocess is not diretcly invoked with the filename anymore,
       but with stdin as input.
    2. Therefore the image file is opened within rauc and streamed to the
       stdin of the tar process with glib streams.
    3. Because we control the pipe, we can count the bytes flowing through
       it, we can emit progress that way.
    4. Because tar can not detect the compression that is used anymore, we
       have to supply the correct decompress option ourself to tar. This is
       done by looking at the filename suffix. (tar does this the same way.)
    
    Signed-off-by: Lars Poeschel <poeschel@lemonage.de>

--- src/update_handler.c
+++ src/update_handler.c
@@ -208,6 +208,97 @@ static gboolean ubifs_ioctl(RaucImage *image, int fd, GError **error)
 	return TRUE;
 }
 
+static gboolean file_to_process_stdin(const gchar *filename, GSubprocess *sproc,
+		GError **error)
+{
+	GError *ierror = NULL;
+	gboolean res = FALSE, ret = TRUE;
+	g_autoptr(GFile) image_file;
+	g_autoptr(GFileInfo) image_info;
+	goffset image_size;
+	GFileInputStream *image_stream;
+	GOutputStream *out_stream;
+	guint8 buffer[4096];
+	gssize in_size = 1;
+	gsize out_size, sum_size = 0;
+	gint last_percent = -1, percent;
+
+	out_stream = g_subprocess_get_stdin_pipe(sproc);
+	if (out_stream == NULL) {
+		g_propagate_prefixed_error(
+				error,
+				ierror,
+				"failed to open tar stdin: ");
+		goto out;
+	}
+
+	image_file = g_file_new_for_path(filename);
+	image_info = g_file_query_info(image_file,
+			G_FILE_ATTRIBUTE_STANDARD_SIZE,
+			G_FILE_QUERY_INFO_NONE,
+			NULL,
+			&ierror);
+	if (image_info == NULL) {
+		g_propagate_error(error, ierror);
+		goto out;
+	}
+
+	image_size = g_file_info_get_size(image_info);
+	image_stream = g_file_read(image_file, NULL, &ierror);
+	if (image_stream == NULL) {
+		g_propagate_prefixed_error(
+				error,
+				ierror,
+				"failed to read image file for copying ");
+		goto out;
+	}
+
+	while ((in_size > 0) && ret) {
+		in_size = g_input_stream_read(G_INPUT_STREAM(image_stream),
+				buffer, 4096, NULL, &ierror);
+		ret = g_output_stream_write_all(out_stream, buffer,
+				in_size, &out_size, NULL, &ierror);
+		sum_size += out_size;
+		percent = sum_size * 100 / image_size;
+		if (percent != last_percent) {
+			last_percent = percent;
+			r_context_set_step_percentage("copy_image", percent);
+		}
+	}
+
+	if ((in_size < 0) || ret == FALSE) {
+		g_propagate_prefixed_error(
+				error,
+				ierror,
+				"failed delivering data through pipe: ");
+		goto out_image_stream;
+	}
+
+	if (g_output_stream_close(out_stream, NULL, &ierror) != TRUE) {
+		g_propagate_prefixed_error(
+				error,
+				ierror,
+				"failed closing output pipe: ");
+		goto out_image_stream;
+	}
+
+	if (g_input_stream_close(G_INPUT_STREAM(image_stream), NULL,
+			&ierror) != TRUE) {
+		g_propagate_prefixed_error(
+				error,
+				ierror,
+				"failed closing image file: ");
+		goto out_image_stream;
+	}
+
+	res = TRUE;
+
+out_image_stream:
+	g_object_unref(image_stream);
+out:
+	return res;
+}
+
 static gboolean copy_raw_image(RaucImage *image, GUnixOutputStream *outstream, gsize len_header_last, GError **error)
 {
 	GError *ierror = NULL;
@@ -1166,6 +1257,45 @@ out:
 	return res;
 }
 
+struct suffix_tar_flag {
+	const char *suffix;
+	const char *tar_flag;
+};
+
+static struct suffix_tar_flag suffixes[] = {
+	{".tar",	NULL},
+	{".gz",		"-z"},
+	{".tgz",	"-z"},
+	{".taz",	"-z"},
+	{".Z",		"-Z"},
+	{".taZ",	"-Z"},
+	{".bz2",	"-j"},
+	{".tbz",	"-j"},
+	{".tbz2",	"-j"},
+	{".tz2",	"-j"},
+	{".lz",		"--lzip"},
+	{".lzma",	"--lzma"},
+	{".tlz",	"--lzma"},
+	{".lzo",	"--lzop"},
+	{".xz",		"-J"},
+	{".txz",	"-J"},
+	{".zst",	"--zstd"},
+	{".tzst",	"--zstd"},
+	{NULL,		NULL}
+};
+
+static const gchar *suffix_to_tar_flag(const gchar *filename)
+{
+	g_return_val_if_fail(filename, NULL);
+
+	for (int i = 0; suffixes[i].suffix != NULL; i++) {
+		if (g_str_has_suffix(filename, suffixes[i].suffix))
+			return suffixes[i].tar_flag;
+	}
+
+	return NULL;
+}
+
 static gboolean untar_image(RaucImage *image, gchar *dest, GError **error)
 {
 	g_autoptr(GSubprocess) sproc = NULL;
@@ -1175,18 +1305,28 @@ static gboolean untar_image(RaucImage *image, gchar *dest, GError **error)
 
 	g_ptr_array_add(args, g_strdup("tar"));
 	g_ptr_array_add(args, g_strdup("xf"));
-	g_ptr_array_add(args, g_strdup(image->filename));
+	g_ptr_array_add(args, g_strdup("-"));
 	g_ptr_array_add(args, g_strdup("-C"));
 	g_ptr_array_add(args, g_strdup(dest));
 	g_ptr_array_add(args, g_strdup("--numeric-owner"));
+	g_ptr_array_add(args, g_strdup(suffix_to_tar_flag(image->filename)));
 	g_ptr_array_add(args, NULL);
 
-	sproc = r_subprocess_newv(args, G_SUBPROCESS_FLAGS_NONE, &ierror);
+	sproc = r_subprocess_newv(args, G_SUBPROCESS_FLAGS_STDIN_PIPE, &ierror);
 	if (sproc == NULL) {
 		g_propagate_prefixed_error(
 				error,
 				ierror,
 				"failed to start tar extract: ");
+		goto out;
+	}
+
+	res =  file_to_process_stdin(image->filename, sproc, &ierror);
+	if (!res) {
+		g_propagate_prefixed_error(
+				error,
+				ierror,
+				"io failure during running tar extract: ");
 		goto out;
 	}
 
