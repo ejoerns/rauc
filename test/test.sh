53e3ff3334186d3cebae1741491f20d3644f256b
Enrico Joerns <ejo@pengutronix.de>
2021-11-22 21:02:16 +0100

    test.sh: abort on already-mounted

--- test/test.sh
+++ test/test.sh
@@ -17,13 +17,12 @@ mkfs.ext4 -F -I 256 $TMPDIR/target-dev > /dev/null
 dd if=/dev/random of=$TMPDIR/test-image bs=1M count=20
 mkfs.ext4 -F -I 256 $TMPDIR/test-image > /dev/null
 
-# simulate installation
-
-# mount and inspect for status file
-mkdir $TMPDIR/mount
 mount -t ext4 -v $TMPDIR/target-dev $TMPDIR/mount
-stat $TMPDIR/mount/rauc.slots || true
 umount -v $TMPDIR/target-dev
+
+# verify not mounted
+LOSETUP=$(losetup -j $TMPDIR/target-dev)
+echo "$LOSETUP" | grep "/dev/loop" && echo "$LOSETUP" && exit 1
 
 # copy content of image
 cat $TMPDIR/test-image > $TMPDIR/target-dev
