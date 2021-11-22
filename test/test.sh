5ffd5971d8a5c970d9dafcb558be9e1a54c4d220
Enrico Joerns <ejo@pengutronix.de>
2021-11-22 21:05:07 +0100

    test.sh: fix

--- test/test.sh
+++ test/test.sh
@@ -17,6 +17,7 @@ mkfs.ext4 -F -I 256 $TMPDIR/target-dev > /dev/null
 dd if=/dev/random of=$TMPDIR/test-image bs=1M count=20
 mkfs.ext4 -F -I 256 $TMPDIR/test-image > /dev/null
 
+mkdir $TMPDIR/mount
 mount -t ext4 -v $TMPDIR/target-dev $TMPDIR/mount
 umount -v $TMPDIR/target-dev
 
@@ -30,6 +31,9 @@ cat $TMPDIR/test-image > $TMPDIR/target-dev
 # remount for writing status file
 mount -t ext4 -v $TMPDIR/target-dev $TMPDIR/mount
 umount -v $TMPDIR/target-dev
+# verify not mounted
+LOSETUP=$(losetup -j $TMPDIR/target-dev)
+echo "$LOSETUP" | grep "/dev/loop" && echo "$LOSETUP" && exit 1
 
 echo "Test run $run done..."
 
