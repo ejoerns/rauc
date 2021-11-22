cd50b9521a9bbc3a4e9932b4e5744b0369084fa6
Enrico Joerns <ejo@pengutronix.de>
2021-11-22 22:13:56 +0100

    test.sh: attempot to provoke FS corruption

--- test/test.sh
+++ test/test.sh
@@ -4,31 +4,51 @@ set -e
 
 echo "Setting up for $1 test runs"
 
-for run in $(seq 1 $1); do
-
-echo "Starting test run $run..."
-
 TMPDIR=$(mktemp -d)
 
 # create target device
-dd if=/dev/zero of=$TMPDIR/target-dev bs=1M count=10
-/sbin/mkfs.ext4 -F -I 256 $TMPDIR/target-dev > /dev/null
+dd if=/dev/zero of=$TMPDIR/target-dev bs=1M count=2000
+mkfs.ext4 -F -I 256 $TMPDIR/target-dev > /dev/null
 # create test image
-dd if=/dev/random of=$TMPDIR/test-image bs=1M count=20
-/sbin/mkfs.ext4 -F -I 256 $TMPDIR/test-image > /dev/null
+dd if=/dev/random of=$TMPDIR/test-image bs=1M count=1800
+mkfs.ext4 -F -I 256 $TMPDIR/test-image > /dev/null
 
+# crate mount mounts
 mkdir $TMPDIR/mount
-mount -t ext4 -v $TMPDIR/target-dev $TMPDIR/mount
-umount -d -v $TMPDIR/mount
-losetup -d $TMPDIR/target-dev || true
+
+# create minimal FS content
+mount -t ext4 $TMPDIR/target-dev $TMPDIR/mount
+mkdir -p /usr/bin /etc /lib /home/root
+touch /usr/bin/file /etc/file /lib/file /home/root/file
+umount $TMPDIR/target-dev
+
+for run in $(seq 1 $1); do
+
+echo "Run $run..."
+
+mount -t ext4 $TMPDIR/target-dev $TMPDIR/mount
+touch $TMPDIR/mount/rauc.slots.bak || true
+umount $TMPDIR/target-dev
 
 # verify not mounted
-LOSETUP=$(losetup -j $TMPDIR/target-dev)
-#echo "LOSETUP"
-echo "$LOSETUP" | grep "/dev/loop" && echo "HIT ISSUE!" && exit 1
+#LOSETUP=$(losetup -j $TMPDIR/target-dev)
+#echo "LOSETUP: $LOSETUP"
+#echo "$LOSETUP" | grep "/dev/loop" && exit 1
 
-echo "Test run $run done..."
+# copy content of image
+cat $TMPDIR/test-image > $TMPDIR/target-dev
 
-rm -rf $TMPDIR
+# remount for writing status file
+mount -t ext4 $TMPDIR/target-dev $TMPDIR/mount
+echo "Status file changed" > $TMPDIR/mount/status.file
+umount $TMPDIR/target-dev
+# verify not mounted
+#LOSETUP=$(losetup -j $TMPDIR/target-dev)
+#echo "LOSETUP: $LOSETUP"
+#echo "$LOSETUP" | grep "/dev/loop" && exit 1
+
+echo "done."
 
 done
+
+rm -rf $TMPDIR
