7ff57bd015b71aa9790fd8c47f7f06f0a052af54
Enrico Joerns <ejo@pengutronix.de>
2021-11-24 08:48:47 +0100

    lslocks

--- test/test.sh
+++ test/test.sh
@@ -69,12 +69,11 @@ ps faux
 # copy content of image
 dd if=$TMPDIR/test-image of=$TMPDIR/target-dev bs=1M conv=fsync >> $LOGFILE 2>&1
 
-sleep 1
-
 # echo if loop associated
 echo "TP@1: $(losetup -j $TMPDIR/target-dev)" >> $LOGFILE
 
 stat $TMPDIR/target-dev
+lslocks
 
 # mount again for writing status file
 flock $TMPDIR/target-dev mount -t ext4 $TMPDIR/target-dev $TMPDIR/mount
