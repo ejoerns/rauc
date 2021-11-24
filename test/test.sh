6e01b1ca150d45d236d282fa61a2d466594cb47a
Enrico Joerns <ejo@pengutronix.de>
2021-11-24 10:45:57 +0100

    this exact state works on debain:buster running on 5.14.9-2 debian kernel

--- test/test.sh
+++ test/test.sh
@@ -30,16 +30,38 @@ dd if=/dev/zero of=$TMPDIR/target-dev-c bs=1M count=50
 mkfs.ext4 -F -I 256 $TMPDIR/target-dev-c > /dev/null
 dd if=/dev/zero of=$TMPDIR/target-dev-d bs=1M count=50
 mkfs.ext4 -F -I 256 $TMPDIR/target-dev-d > /dev/null
+dd if=/dev/zero of=$TMPDIR/target-dev-e bs=1M count=50
+mkfs.ext4 -F -I 256 $TMPDIR/target-dev-e > /dev/null
+dd if=/dev/zero of=$TMPDIR/target-dev-f bs=1M count=50
+mkfs.ext4 -F -I 256 $TMPDIR/target-dev-f > /dev/null
+dd if=/dev/zero of=$TMPDIR/target-dev-g bs=1M count=50
+mkfs.ext4 -F -I 256 $TMPDIR/target-dev-g > /dev/null
+dd if=/dev/zero of=$TMPDIR/target-dev-h bs=1M count=50
+mkfs.ext4 -F -I 256 $TMPDIR/target-dev-h > /dev/null
+dd if=/dev/zero of=$TMPDIR/target-dev-i bs=1M count=50
+mkfs.ext4 -F -I 256 $TMPDIR/target-dev-i > /dev/null
 
 mkdir $TMPDIR/mount-a
 mkdir $TMPDIR/mount-b
 mkdir $TMPDIR/mount-c
 mkdir $TMPDIR/mount-d
+mkdir $TMPDIR/mount-e
+mkdir $TMPDIR/mount-f
+mkdir $TMPDIR/mount-g
+mkdir $TMPDIR/mount-h
+mkdir $TMPDIR/mount-i
 
 mount -t ext4 $TMPDIR/target-dev-a $TMPDIR/mount-a
 mount -t ext4 $TMPDIR/target-dev-b $TMPDIR/mount-b
 mount -t ext4 $TMPDIR/target-dev-c $TMPDIR/mount-c
 mount -t ext4 $TMPDIR/target-dev-d $TMPDIR/mount-d
+mount -t ext4 $TMPDIR/target-dev-e $TMPDIR/mount-e
+mount -t ext4 $TMPDIR/target-dev-f $TMPDIR/mount-f
+mount -t ext4 $TMPDIR/target-dev-g $TMPDIR/mount-g
+mount -t ext4 $TMPDIR/target-dev-h $TMPDIR/mount-h
+mount -t ext4 $TMPDIR/target-dev-i $TMPDIR/mount-i
+
+losetup -a
 
 # create minimal FS content
 #mount -t ext4 $TMPDIR/target-dev $TMPDIR/mount
@@ -47,7 +69,7 @@ mount -t ext4 $TMPDIR/target-dev-d $TMPDIR/mount-d
 #touch /usr/bin/file /etc/file /lib/file /home/root/file
 #umount $TMPDIR/target-dev
 
-ps faux
+#ps faux
 
 for run in $(seq 1 $1); do
 
@@ -64,7 +86,7 @@ umount $TMPDIR/target-dev
 # echo if loop associated
 echo "TP@0: $(losetup -j $TMPDIR/target-dev)" >> $LOGFILE
 
-ps faux
+#ps faux
 
 # copy content of image
 dd if=$TMPDIR/test-image of=$TMPDIR/target-dev bs=1M conv=fsync >> $LOGFILE 2>&1
@@ -72,11 +94,11 @@ dd if=$TMPDIR/test-image of=$TMPDIR/target-dev bs=1M conv=fsync >> $LOGFILE 2>&1
 # echo if loop associated
 echo "TP@1: $(losetup -j $TMPDIR/target-dev)" >> $LOGFILE
 
-stat $TMPDIR/target-dev
-lslocks
+#stat $TMPDIR/target-dev
+#lslocks
 
 # find non-overlapping device!
-LOOPDEV=$(losetup --find -L -r --show $TMPDIR/target-dev)
+LOOPDEV=$(losetup --find -L --show $TMPDIR/target-dev)
 echo "mounting $LOOPDEV"
 mount -t ext4 $LOOPDEV $TMPDIR/mount
 
@@ -93,6 +115,11 @@ umount $TMPDIR/mount-a
 umount $TMPDIR/mount-b
 umount $TMPDIR/mount-c
 umount $TMPDIR/mount-d
+umount $TMPDIR/mount-e
+umount $TMPDIR/mount-f
+umount $TMPDIR/mount-g
+umount $TMPDIR/mount-h
+umount $TMPDIR/mount-i
 
 # dump log
 cat $LOGFILE
